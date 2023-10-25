d1bea7455e50a5ee0f33ce6cbd16d2452f8bf0f7
CompiledError <70342656+CompiledError@users.noreply.github.com>
2023-10-25 13:56:22 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/settlersofcatan/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.settlersofcatan;
+
+import android.os.Bundle;
+
+import com.example.game.GameFramework.GameMainActivity;
+import com.example.game.GameFramework.LocalGame;
+import com.example.game.GameFramework.gameConfiguration.GameConfig;
+import com.example.game.GameFramework.infoMessage.GameState;
+
+public class MainActivity extends GameMainActivity {
+
+    @Override
+    public GameConfig createDefaultConfig() {
+        return null;
+    }
+
+    @Override
+    public LocalGame createLocalGame(GameState gameState) {
+        return null;
+    }
+}
