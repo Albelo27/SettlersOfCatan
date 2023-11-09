00a91f938da409444cf4903d362bb9b15c7de6c6
Connersm <143753907+Connersm@users.noreply.github.com>
2023-11-08 19:52:52 -0800

    I touched the Butt

--- app/src/main/java/com/example/util/DoNotTouch.java
+++ app/src/main/java/com/example/util/DoNotTouch.java
@@ -3,16 +3,9 @@ package com.example.util;
 public class DoNotTouch {
     public final float X0 = 732; /*i touched it
                                  -Reiss P.S. pls don't break my wrists ty.*/
-    public final float X1 = 785;
-    public final float X2 = 838;
-    public final float X3 = 891;
-    public final float X4 = 944;
-    public final float X5 = 997;
-    public final float X6 = 1050;
-    public final float X7 = 1103;
-    public final float X8 = 1156;
-    public final float X9 = 1209;
-    public final float X10 = 1262;
+    public final float X1 = 785; public final float X2 = 838; public final float X3 = 891; public final float X4 = 944; public final float X5 = 997; public final float X6 = 1050; public final float X7 = 1103; public final float X8 = 1156; public final float X9 = 1209; public final float X10 = 1262;
+
+    /* I touched it */
     public final float Y0 = 0;
     public final float Y1 = 31;
     public final float Y2 = 93;
