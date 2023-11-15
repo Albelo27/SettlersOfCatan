8c1f04a2e2b7c21941ddea2ab192f774225cad44
dumplingg <r4rero@gmail.com>
2023-11-14 17:52:03 -0800

    CatanLocalGame makeMove method

--- app/src/main/java/com/example/settlersofcatan/BuildAction.java
+++ app/src/main/java/com/example/settlersofcatan/BuildAction.java
@@ -4,12 +4,52 @@ import com.example.game.GameFramework.actionMessage.GameAction;
 import com.example.game.GameFramework.players.GamePlayer;
 
 public class BuildAction extends GameAction {
+
+    private String building;
+    private float X;
+    private float Y;
+    private float Z;
+    private float Q;
+
     /**
-     * constructor for GameAction
+     * constructor for GameAction when building a city or settlement
      *
      * @param player the player who created the action
+     * @param building type of building being built (city or settlement)
+     * @param X x coordinate of settlement
+     * @param Y y coordinate of settlement
      */
-    public BuildAction(GamePlayer player, String building) {
+    public BuildAction(GamePlayer player, String  building, float X, float Y) {
         super(player);
+    }
+    /**
+     * constructor for GameAction when building a road
+     * @param player the player who created the action
+     * @param building type of building being built (road)
+     * @param X x coordinate of the road
+     * @param Y y coordinate of the road
+     * @param Z z coordinate of the road
+     * @param Q q coordinate of the road
+     */
+    public BuildAction(GamePlayer player, String building, float X, float Y, float Z, float Q) {super(player); }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public float getX() {
+        return X;
+    }
+
+    public float getY() {
+        return Y;
+    }
+
+    public float getZ() {
+        return Z;
+    }
+
+    public float getQ() {
+        return Q;
     }
 }
--- app/src/main/java/com/example/settlersofcatan/PlayDCAction.java
+++ app/src/main/java/com/example/settlersofcatan/PlayDCAction.java
@@ -4,12 +4,28 @@ import com.example.game.GameFramework.actionMessage.GameAction;
 import com.example.game.GameFramework.players.GamePlayer;
 
 public class PlayDCAction extends GameAction {
+
+    private int devCardPlayed;
+    private int resourceID;
     /**
-     * constructor for GameAction
+     * constructor for creating a PlayDCAction to be sent to the LocalGame with received
+     * parameters
      *
      * @param player the player who created the action
+     * @param cardPlayed the type of development card that was played
+     * @param resID the type of resource chosen by the player for applicable DCs
      */
-    public PlayDCAction(GamePlayer player, int cardPlayed) {
+    public PlayDCAction(GamePlayer player, int cardPlayed, int resID) {
         super(player);
+    }
+
+
+
+    public int getDCPlayed() {
+        return devCardPlayed;
+    }
+
+    public int getResID() {
+        return resourceID;
     }
 }
